25cd2d792faf8807e9f00639614e518f9d326207
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-02-14 00:23:48 +0530

    Added Authcontext for overall app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthContextContainer from "@/Context/AuthContextContainer";
 
 let theme = createTheme({
   palette: {
@@ -10,15 +11,17 @@ let theme = createTheme({
   },
 });
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <AuthContextContainer>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </AuthContextContainer>
   );
 }
