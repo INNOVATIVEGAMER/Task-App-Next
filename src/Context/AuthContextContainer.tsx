37fddb775b0613386f615ca32e051946e109a4e5
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-02-15 21:39:46 +0530

    Refactored auth logic code

--- src/Context/AuthContextContainer.tsx
+++ src/Context/AuthContextContainer.tsx
@@ -25,29 +25,29 @@ const AuthContextContainer = ({ children }: IProps) => {
   const router = useRouter();
 
   useEffect(() => {
+    // Get the auth token from local storage and session storage
     const LS_AUTH_TOKEN = localStorage.getItem("authToken");
     const SS_AUTH_TOKEN = sessionStorage.getItem("authToken");
-    if (LS_AUTH_TOKEN && !authToken) setAuthToken(LS_AUTH_TOKEN);
-    if (SS_AUTH_TOKEN && !authToken) setAuthToken(SS_AUTH_TOKEN);
 
-    if (rememberMe && !LS_AUTH_TOKEN && authToken) {
-      localStorage.setItem("authToken", authToken);
-      sessionStorage.removeItem("authToken");
+    // If there is an auth token in storage and it hasn't been set in state yet, set it in state
+    if (!authToken && (LS_AUTH_TOKEN || SS_AUTH_TOKEN)) {
+      setAuthToken(LS_AUTH_TOKEN || SS_AUTH_TOKEN);
     }
-
-    if (!rememberMe && !LS_AUTH_TOKEN && !SS_AUTH_TOKEN && authToken) {
-      sessionStorage.setItem("authToken", authToken);
-      localStorage.removeItem("authToken");
+    // If there is an auth token in state, store it in local or session storage based on the rememberMe value
+    else if (authToken) {
+      if (rememberMe) {
+        localStorage.setItem("authToken", authToken);
+        sessionStorage.removeItem("authToken");
+      } else {
+        sessionStorage.setItem("authToken", authToken);
+        localStorage.removeItem("authToken");
+      }
     }
-
-    if (
-      !SS_AUTH_TOKEN &&
-      !LS_AUTH_TOKEN &&
-      !authToken &&
-      !AUTH_PATHS.includes(router.pathname)
-    )
+    // If there is no auth token in storage, state, or pathname is not in the authorized paths, redirect to the sign-in page
+    else if (!AUTH_PATHS.includes(router.pathname)) {
       router.push("/signin");
-  }, [authToken, rememberMe]);
+    }
+  }, [authToken, rememberMe, router.pathname]);
 
   return (
     <AuthContext.Provider
