5f1505438c749fc5a398a7314e5429fc07a338f2
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-03-28 04:00:27 +0530

    loading logic with edge cases handeled

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,22 +3,24 @@ import Info from "@/components/profile/Info";
 import { QUERY_KEYS } from "@/Constants/TanstackConstants";
 import { useAuth } from "@/Context/Auth";
 import ProfilePageLayout from "@/layout/ProfilePageLayout";
-import { Box, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useCallback } from "react";
 
 interface IProps {}
 
+const getProfile = (authToken: string | null) => {
+  if (!authToken) return null;
+
+  return GetProfileUser({ AUTH_TOKEN: authToken });
+};
+
 const ProfilePage = ({}: IProps) => {
   const { authToken } = useAuth();
-  const getProfile = () => {
-    if (!authToken) return null;
 
-    return GetProfileUser({ AUTH_TOKEN: authToken });
-  };
-  const { data } = useQuery({
-    queryKey: [QUERY_KEYS.USER_PROFILE],
-    queryFn: () => getProfile(),
+  const { data, isFetching, isLoading, ...other } = useQuery({
+    queryKey: [QUERY_KEYS.USER_PROFILE, authToken],
+    queryFn: () => getProfile(authToken),
   });
 
   return (
@@ -26,7 +28,10 @@ const ProfilePage = ({}: IProps) => {
       <Box my="2rem">
         <Typography variant="h2">Profile Page</Typography>
       </Box>
-      <Info name={data.name} age={data.age} email={data.email} />
+      {(isFetching || isLoading) && <CircularProgress />}
+      {!isFetching && !isLoading && data && (
+        <Info name={data.name} age={data.age} email={data.email} />
+      )}
     </ProfilePageLayout>
   );
 };
