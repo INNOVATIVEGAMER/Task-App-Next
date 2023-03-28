b3b0acd6f06983d01bd063cdc1f7f0fb75d9525c
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-03-29 02:33:33 +0530

    Added logoutAll button

--- src/components/profile/Info.tsx
+++ src/components/profile/Info.tsx
@@ -1,8 +1,18 @@
-import { Box, Button, Grid, Paper, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import EditInfoSection from "./EditInfoSection";
 import LogoutButton from "../auth/LogoutButton";
+import LogoutAllButton from "../auth/LogoutAllButton";
+import KeyIcon from "@mui/icons-material/Key";
 
 interface IProps {
   name: string;
@@ -18,47 +28,74 @@ const Info = ({ age, email, name }: IProps) => {
   };
 
   return (
-    <Paper>
-      <Grid container spacing={2} alignItems="center" p="1rem">
-        <Grid item xs={8}>
-          <Typography color="primary" variant="h5">
-            Public Info
-          </Typography>
-        </Grid>
-        {!editMode && (
-          <Grid item xs={4}>
-            <Box display="flex" gap="1rem" alignItems="center">
-              <Button
-                onClick={() => seteditMode(true)}
-                endIcon={<EditIcon color="info" fontSize="small" />}
-                color="info"
-                variant="outlined"
-              >
-                Edit
-              </Button>
-              <LogoutButton />
-            </Box>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={8}>
+            <Typography color="primary" variant="h5">
+              Public Info
+            </Typography>
           </Grid>
-        )}
-        <Grid item xs={10}>
           {!editMode && (
-            <Stack spacing={2}>
-              <Typography>Name -- {name}</Typography>
-              <Typography>Email -- {email}</Typography>
-              <Typography>Age -- {age}</Typography>
-            </Stack>
+            <Grid item xs={4}>
+              <Box display="flex" gap="1rem" alignItems="center">
+                <Button
+                  onClick={() => seteditMode(true)}
+                  endIcon={<EditIcon color="info" fontSize="small" />}
+                  color="info"
+                  variant="outlined"
+                >
+                  Edit
+                </Button>
+                <LogoutButton />
+              </Box>
+            </Grid>
           )}
-          {editMode && (
-            <EditInfoSection
-              age={age}
-              email={email}
-              name={name}
-              closeEditMode={closeEditMode}
-            />
-          )}
+          <Grid item xs={10}>
+            {!editMode && (
+              <Stack spacing={2}>
+                <Typography>Name -- {name}</Typography>
+                <Typography>Email -- {email}</Typography>
+                <Typography>Age -- {age}</Typography>
+              </Stack>
+            )}
+            {editMode && (
+              <EditInfoSection
+                age={age}
+                email={email}
+                name={name}
+                closeEditMode={closeEditMode}
+              />
+            )}
+          </Grid>
         </Grid>
       </Grid>
-    </Paper>
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12}>
+            <Typography color="primary" variant="h5">
+              Security Features
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Box display="flex" gap="1rem" alignItems="center">
+              <Button
+                onClick={() => console.log("functionality to be added")}
+                endIcon={<KeyIcon color="inherit" fontSize="small" />}
+                color="secondary"
+                variant="outlined"
+              >
+                Change Password
+              </Button>
+              <LogoutAllButton />
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
